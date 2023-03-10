f6e065055b649a96e4f3a60d2c929cf926d4518e
JHON CESAR PABLO <jhncsrpbl@gmail.com>
2023-03-10 22:21:17 +0800

    Added users page.

---
+++ jobtech-web/app/users/page.tsx
@@ -0,0 +1,11 @@
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Users',
+};
+
+const UsersPage = () => {
+  return <div>UsersPage</div>;
+};
+
+export default UsersPage;
