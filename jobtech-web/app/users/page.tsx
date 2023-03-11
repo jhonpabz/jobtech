e0e26a33882093f5a776f9467b98e75430153ac8
JHON CESAR PABLO <jhncsrpbl@gmail.com>
2023-03-11 08:10:08 +0800

    Added getAllusers function.

--- jobtech-web/app/users/page.tsx
+++ jobtech-web/app/users/page.tsx
@@ -1,10 +1,12 @@
 import type { Metadata } from 'next';
+import getAllUsers from '@/lib/getAllUsers';
 
 export const metadata: Metadata = {
   title: 'Users',
 };
 
 const UsersPage = () => {
+  const usersData: Promise<> = getAllUsers();
   return <div>UsersPage</div>;
 };
 
