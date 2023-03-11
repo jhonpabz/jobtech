9eb844097455919f5e56866bd54bf46cdfc972d0
JHON CESAR PABLO <jhncsrpbl@gmail.com>
2023-03-11 17:24:17 +0800

    Display fetched user data and user posts data.

--- jobtech-web/app/users/[userId]/page.tsx
+++ jobtech-web/app/users/[userId]/page.tsx
@@ -1,11 +1,31 @@
+import getUser from '@/lib/getUser';
+import getUserPosts from '@/lib/getUserPosts';
+import { Suspense } from 'react';
+import UserPosts from './component/UserPosts';
+
 type Params = {
   params: {
     userId: string;
   };
 };
 
-const UserPage = ({ params: { userId } }: Params) => {
-  return <div>UserPage</div>;
+const UserPage = async ({ params: { userId } }: Params) => {
+  const userData: Promise<User> = getUser(userId);
+  const userPostsData: Promise<Post[]> = getUserPosts(userId);
+
+  // const [user, userPosts] = await Promise.all([userData, userPostsData]);
+
+  const user = await userData;
+
+  return (
+    <>
+      <h2>{user.name}</h2>
+      <br />
+      <Suspense fallback={<h2>Loading...</h2>}>
+        <UserPosts promise={userPostsData} />
+      </Suspense>
+    </>
+  );
 };
 
 export default UserPage;
