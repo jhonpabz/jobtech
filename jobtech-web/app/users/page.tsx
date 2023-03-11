57605439bf6a58afe2bad516f04d06f725426b0d
JHON CESAR PABLO <jhncsrpbl@gmail.com>
2023-03-11 08:27:17 +0800

    Map fetched data.

--- jobtech-web/app/users/page.tsx
+++ jobtech-web/app/users/page.tsx
@@ -1,13 +1,36 @@
 import type { Metadata } from 'next';
 import getAllUsers from '@/lib/getAllUsers';
+import Link from 'next/link';
 
 export const metadata: Metadata = {
   title: 'Users',
 };
 
-const UsersPage = () => {
+const UsersPage = async () => {
   const usersData: Promise<User[]> = getAllUsers();
-  return <div>UsersPage</div>;
+
+  const users = await usersData;
+  console.log('users: ', users);
+
+  const content = (
+    <div>
+      <h1>
+        <Link href="/">Back to Home</Link>
+      </h1>
+      <br />
+      {users.map((user) => {
+        return (
+          <>
+            <p key={user.id}>
+              <Link href={`/users${user.id}`}>{user.name}</Link>
+            </p>
+          </>
+        );
+      })}
+    </div>
+  );
+
+  return content;
 };
 
 export default UsersPage;
