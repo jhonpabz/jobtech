3e841cee847af531c847a20df83eb597f534b4d6
JHON CESAR PABLO <jhncsrpbl@gmail.com>
2023-03-11 09:46:29 +0800

    Added getUser function.

---
+++ jobtech-web/lib/getUser.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const getUser = async (userId: string) => {
+  const res = await fetch(
+    `https://jsonplaceholder.typicode.com/users/${userId}`
+  );
+
+  if (!res.ok) throw new Error('Failed to fetch user');
+  return res.json();
+};
+
+export default getUser;
