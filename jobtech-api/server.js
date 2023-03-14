ba46c3fb4b15f94c32eb2a21d8a552105d6602c0
JHON CESAR PABLO <jhncsrpbl@gmail.com>
2023-03-14 21:25:54 +0800

    Modified server.

--- jobtech-api/server.js
+++ jobtech-api/server.js
@@ -3,6 +3,8 @@ const app = express();
 import dotenv from 'dotenv';
 dotenv.config();
 
+import connectDB from './db/connect.js';
+
 // middleware
 import notFoundMiddleware from './middleware/notFound.js';
 import errorHandlerMiddleware from './middleware/errorHandler.js';
@@ -17,6 +19,15 @@ app.use(errorHandlerMiddleware);
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => {
-  console.log(`Server is listening to port ${port}`);
-});
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URL);
+    app.listen(port, () => {
+      console.log(`Server is listening to port ${port}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
