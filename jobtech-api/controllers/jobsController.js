8750112eab09d0a9e8730687fab12e9debca9f4d
JHON CESAR PABLO <jhncsrpbl@gmail.com>
2023-03-16 17:23:01 +0800

    Added jobsController.

---
+++ jobtech-api/controllers/jobsController.js
@@ -0,0 +1,21 @@
+const createJob = (req, res) => {
+  res.send('Create job');
+};
+
+const deleteJob = (req, res) => {
+  res.send('Delete job');
+};
+
+const getAllJobs = (req, res) => {
+  res.send('Get all jobs');
+};
+
+const updateJob = (req, res) => {
+  res.send('Create job');
+};
+
+const showStats = (req, res) => {
+  res.send('Show stats');
+};
+
+export { createJob, deleteJob, getAllJobs, updateJob, showStats };
