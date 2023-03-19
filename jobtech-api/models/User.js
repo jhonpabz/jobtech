e2f96ac842fa2f9b1592a456b86f3ef0e5866466
JHON CESAR PABLO <jhncsrpbl@gmail.com>
2023-03-19 09:10:20 +0800

    Installed validator package and update validation to email.

--- jobtech-api/models/User.js
+++ jobtech-api/models/User.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import validator from 'validator';
 
 const UserSchema = new mongoose.Schema({
   name: {
@@ -11,6 +12,11 @@ const UserSchema = new mongoose.Schema({
   email: {
     type: String,
     required: [true, 'Email is required'],
+    validate: {
+      validator: validator.isEmail,
+      message: 'Please provide a valid email',
+      },
+    },
     unique: true,
   },
   password: {
