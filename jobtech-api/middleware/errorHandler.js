40bed313b29f2c26f6f58f7bb102fc1940c5d54a
JHON CESAR PABLO <jhncsrpbl@gmail.com>
2023-03-20 20:58:51 +0800

    Set error message for duplicate email.

--- jobtech-api/middleware/errorHandler.js
+++ jobtech-api/middleware/errorHandler.js
@@ -15,6 +15,11 @@ const errorHandlerMiddleware = (err, req, res, next) => {
       .join(',');
   }
 
+  // Check if duplicate email. Set error message for dup email
+  if (err.code && err.code === 11000) {
+    defaultError.statusCode = StatusCodes.BAD_REQUEST;
+    defaultError.msg = `${Object.keys(err.keyValue)} field has to be unique `;
+  }
   // res.status(defaultError.statusCode).json({ msg: err });
   res.status(defaultError.statusCode).json({ msg: defaultError.msg });
 };
