564ec75f98df02148d7df06a55b3cb3aebcf1a69
JHON CESAR PABLO <jhncsrpbl@gmail.com>
2023-03-25 20:33:14 +0800

    Check email duplicate.

--- jobtech-api/controllers/authController.js
+++ jobtech-api/controllers/authController.js
@@ -5,8 +5,13 @@ import { BadRequestError } from '../errors/index.js';
 const register = async (req, res) => {
   const { name, email, password } = req.body;
 
-  if (!name || email || !password) {
+  if (!name || !email || !password) {
     throw new BadRequestError('Please provide all values');
+  }
+
+  const userAlreadyExists = await User.findOne({ email });
+  if (userAlreadyExists) {
+    throw new BadRequestError('Email already in use');
   }
   const user = await User.create({ name, email, password });
   res.status(StatusCodes.CREATED).json({ user });
