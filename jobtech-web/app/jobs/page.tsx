8dfb21932b44889e5c12dec4d34cfb401c724601
JHON CESAR PABLO <jhncsrpbl@gmail.com>
2023-04-02 16:54:29 +0800

    Fetch data.

--- jobtech-web/app/jobs/page.tsx
+++ jobtech-web/app/jobs/page.tsx
@@ -1,7 +1,23 @@
+'use client';
 import Link from 'next/link';
+import { useEffect } from 'react';
 
 const Jobs = () => {
   // throw new Error('Error zxcv');
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch('http://localhost:5000/');
+      const data = await response.json();
+      console.log('data: ', data);
+    } catch (error) {
+      console.log('error', error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
   return (
     <>
       <main>
