8b40ea340abacfbde1550230146c7c40933ff1d8
JHON CESAR PABLO <jhncsrpbl@gmail.com>
2023-04-10 13:37:25 +0800

    Added form container component.

---
+++ jobtech-web/app/components/Forms/FormContainer/FormContainer.tsx
@@ -0,0 +1,11 @@
+import { FormContainerInterface } from './FormContainerInterface';
+
+const FormContainer = ({ children, onSubmit }: FormContainerInterface) => {
+  return (
+    <>
+      <form onSubmit={onSubmit}>{children}</form>
+    </>
+  );
+};
+
+export default FormContainer;
