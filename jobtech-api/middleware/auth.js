b9b19d5220179b6350b68af4570f8d1ca7385e6c
JHON CESAR PABLO <jhncsrpbl@gmail.com>
2023-05-07 13:01:18 +0800

    Throw an 401 error if auth header is missing.

--- jobtech-api/middleware/auth.js
+++ jobtech-api/middleware/auth.js
@@ -1,7 +1,13 @@
+import jwt from 'jsonwebtoken';
+import { UnAuthenticatedError } from '../errors/index.js';
+
 const auth = async (req, res, next) => {
-  const headers = req.headers;
+  // Check header
   const authHeader = req.headers.authorization;
-  console.log('headers: ', headers);
+
+  if (!authHeader) {
+    throw new UnAuthenticatedError('Authentication Invalid');
+  }
   console.log('authHeader: ', authHeader);
   next();
 };
