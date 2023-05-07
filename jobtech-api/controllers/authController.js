7195be2ccfa448567de1e1eeaa07731435ee6f76
JHON CESAR PABLO <jhncsrpbl@gmail.com>
2023-05-07 14:03:52 +0800

    Functioning update user.

--- jobtech-api/controllers/authController.js
+++ jobtech-api/controllers/authController.js
@@ -51,7 +51,33 @@ const login = async (req, res) => {
   });
 };
 const updateUser = async (req, res) => {
-  res.send('update User');
+  const { email, name, lastName, location } = req.body;
+  if (!email || !name || !lastName || !location) {
+    throw new BadRequestError('Please provide all values');
+  }
+
+  const user = await User.findOne({ _id: req.user.userId });
+
+  user.email = email;
+  user.name = name;
+  user.lastName = lastName;
+  user.location = location;
+
+  await user.save();
+
+  // various setups
+  // in this case only id
+  // if  other properties included, must re-generate
+
+  // Generate new token to pass
+  const token = user.createJWT();
+  res.status(StatusCodes.OK).json({
+    user,
+    token,
+    location: user.location,
+  });
+
+  res.send('Updated user');
 };
 
 export { register, login, updateUser };
