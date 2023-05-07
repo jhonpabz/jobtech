d1ebbfa2956a961d578de2758ea3c93a0eb4aefd
JHON CESAR PABLO <jhncsrpbl@gmail.com>
2023-05-07 13:20:23 +0800

    Completed auth.

--- jobtech-api/middleware/auth.js
+++ jobtech-api/middleware/auth.js
@@ -5,11 +5,21 @@ const auth = async (req, res, next) => {
   // Check header
   const authHeader = req.headers.authorization;
 
-  if (!authHeader) {
+  if (!authHeader || !authHeader.startsWith('Bearer')) {
     throw new UnAuthenticatedError('Authentication Invalid');
   }
-  console.log('authHeader: ', authHeader);
-  next();
+
+  const token = authHeader.split(' ')[1];
+
+  try {
+    const payload = jwt.verify(token, process.env.JWT_SECRET);
+    console.log('payload: ', payload);
+    //Attach the user request object
+    req.user = payload;
+    next();
+  } catch (error) {
+    throw new UnAuthenticatedError('Authentication Invalid');
+  }
 };
 
 export default auth;
