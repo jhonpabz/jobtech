7cf24528e530bd4dc3898ddecaed0c3aad0a2854
JHON CESAR PABLO <jhncsrpbl@gmail.com>
2023-05-07 18:57:07 +0800

    Added barrel export for form component.

---
+++ vite-web/src/components/Forms/index.ts
@@ -0,0 +1,4 @@
+export * from './Buttons/ButtonComponent';
+export * from './EmailField/EmailFieldComponent';
+export * from './TextField/TextFieldComponent';
+export * from './FormContainer/FormContainer';
